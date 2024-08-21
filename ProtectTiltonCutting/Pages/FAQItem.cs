362503d1ce3b0dc88e755c91a484dc7b0be18099
Simon Sattin <ssattin@gmail.com>
2024-08-21 13:27:14 +0100

    updated for stop tiptoe details

---
+++ ProtectTiltonCutting/Pages/FAQItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ProtectTiltonCutting.Pages
+{
+    public class FAQItem
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public bool IsOpen { get; set; } // To track if the FAQ is open or closed
+    }
+}
